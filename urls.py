0c02e95c9cb4a1f91d9e82cae50e101495d97e14
Alexander Schapov <alexanderad@gmail.com>
2011-07-30 17:13:35 +0300

    Revert "index app, index .urls and welcome page -- hello world commit"
    This reverts commit 4304c09773f70aaf576608dcb871fc23fc63057d.

--- urls.py
+++ urls.py
@@ -3,12 +3,11 @@ from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 
 urlpatterns = patterns('',
-    #(r'^index/', include('alice.core.urls')),
-    (r'', 'index.views.welcome'),
 )
 
 # old school MEDIA
 if settings.DEBUG:
     urlpatterns += patterns('',
-        (r'^(static)/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+        (r'^(static)/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT}),
     )
