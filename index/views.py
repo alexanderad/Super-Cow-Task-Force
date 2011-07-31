cb5e9dfd9598486bb0a5f85c67f6cd0f40e7552f
Alexander Schapov <alexanderad@gmail.com>
2011-07-31 13:15:21 +0300

    index page && errorlist classes

--- index/views.py
+++ index/views.py
@@ -2,6 +2,11 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404 
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django import forms
+
+class QuickTestCheckForm(forms.Form):
+    """ check form """
+    url = forms.URLField(required=True, widget=forms.TextInput(attrs={"class": "link-input"}))
 
 def quick_test(request):
     """ quick test page """
@@ -9,4 +14,11 @@ def quick_test(request):
 
 def index(request):
     """ index page """
-    return render_to_response('index.html', {}, context_instance=RequestContext(request))
+    form = None
+    if "POST" == request.method:
+        form = QuickTestCheckForm(request.POST)
+        if form.is_valid():
+            assert False, "form is valid"            
+    if form is None:
+        form = QuickTestCheckForm()
+    return render_to_response('index.html', {"form": form}, context_instance=RequestContext(request))
